2abf8101197a588dcc45298b1cf089aa5a1264c1
Danilo Miranda <danilo.miranda@tecnova.cl>
2025-01-22 11:38:42 -0300

    create function

---
+++ dotnetfntest/src/Function/Program.cs
@@ -0,0 +1,14 @@
+using Fnproject.Fn.Fdk;
+
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("Function.Tests")]
+namespace Function {
+	class Greeter {
+		public string greet(string input) {
+			return string.Format("Hello {0}!",
+				string.IsNullOrEmpty(input) ? "World" : input.Trim());
+		}
+
+		static void Main(string[] args) { Fdk.Handle(args[0]); }
+	}
+}
